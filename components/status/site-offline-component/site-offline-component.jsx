9f6de854380e77caeff298cf34a2fd83b26f416d
mydogspies <mydogspies@zoho.com>
2021-04-28 14:53:42 +0200

    Added some of the logic for an offline landing page

---
+++ components/status/site-offline-component/site-offline-component.jsx
@@ -0,0 +1,28 @@
+// This is the default page in case the site is marked as offline.
+
+import styles from "../../../styles/Home.module.css";
+import Head from "next/head";
+
+const OfflinePage = () => {
+
+    return (
+        <div className={styles.container}>
+
+            <Head>
+                <title>Bringbackdada.com - fine art body and portrait photography - site offline</title>
+            </Head>
+
+            <main className={styles.main}>
+                <div>
+                    <h3>
+                        This site is under some serious re-construction and will be up in late spring 2021
+                    </h3>
+                </div>
+
+            </main>
+
+        </div>
+    );
+};
+
+export default OfflinePage;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,64 @@
 import Head from 'next/head'
+import Offline from '../components/status/site-offline-component/site-offline-component'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Bringbackdada.com - fine art body and portrait photography</title>
-      </Head>
-        <h3>
-          This site is under some serious construction and we will be back up in flash!
-        </h3>
-      <main className={styles.main}>
+export default function Home({siteStatus}) {
 
-      </main>
-    </div>
-  )
+    if (siteStatus) {
+
+        return (
+            <div className={styles.container}>
+
+                <Head>
+                    <title>Bringbackdada.com - fine art body and portrait photography</title>
+                </Head>
+
+                <main className={styles.main}>
+                    <div>
+                        <h3>
+                            SITE ONLINE!
+                        </h3>
+                    </div>
+
+                    <div>
+                        <h4>Site online: {siteStatus.toString()}</h4>
+                    </div>
+                </main>
+
+            </div>
+        );
+
+    } else {
+
+        return (
+            <Offline />
+        );
+
+    }
+
+};
+
+export async function getStaticProps() {
+
+    try {
+        const res = await fetch(process.env.DB_API + '/api/v1/status');
+        const status = await res.json();
+        const siteStatus = status.online;
+        return {
+            props: {
+                siteStatus
+            },
+            revalidate: 10, // In seconds
+        };
+    } catch (e) {
+        console.log(e); // TODO refactor to Morgan
+        const siteStatus = false;
+        return {
+            props: {
+                siteStatus
+            },
+            revalidate: 10, // In seconds
+        };
+    }
+
 }
